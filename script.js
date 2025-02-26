0a65fe8554c5450e286a38e7c2fac60091dd02a9
Ahmed Yassine <jedidiahmedyassine07@gmail.com>
2025-02-26 14:22:22 +0100

    Start JS for the forum page, fix images for the homepage and BMW, Audi, and VW pages. Adjust the BMW Series 4 page to automatically scroll images and add a photo background

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('btn-confirmation').addEventListener('click', function(event) {
+    event.preventDefault();  // Empêche la soumission traditionnelle du formulaire
+
+    var nom = document.forms['rentalForm']['nom'].value;
+    var prenom = document.forms['rentalForm']['prenom'].value;
+    var email = document.forms['rentalForm']['email'].value;
+    var tel = document.forms['rentalForm']['tel'].value;
+    var dateDebut = document.forms['rentalForm']['date_debut'].value;
+    var duree = document.forms['rentalForm']['duree'].value;
+    var idVoiture = document.forms['rentalForm']['id_voiture'].value;
+
+    if (nom && prenom && email && tel && dateDebut && duree && idVoiture) {
+        alert('Merci ' + prenom + ' ' + nom + ' !\n' +
+              'Votre réservation est confirmée.\n' +
+              'Détails :\n' +
+              'Email: ' + email + '\n' +
+              'Téléphone: ' + tel + '\n' +
+              'Date de début: ' + dateDebut + '\n' +
+              'Durée: ' + duree + ' jours\n' +
+              'Voiture ID: ' + idVoiture);
+    } else {
+        alert('Veuillez remplir tous les champs.');
+    }
+});
